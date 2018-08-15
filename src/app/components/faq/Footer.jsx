17c69d4d553bddf1e84336eee49182e3b81e1f93
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-08-15 10:09:26 +0300

    faq footer work

--- src/app/components/faq/Footer.jsx
+++ src/app/components/faq/Footer.jsx
@@ -1,11 +1,33 @@
 import React, {PureComponent} from 'react';
+import Flex from '../Flex/Flex';
+import styled from 'styled-components';
+
+const Text = styled.p`
+    color: #393636;	
+    font-family: "SF Pro Text", "Roboto", sans-serif;	
+    font-size: 16px;	
+    font-weight: 500;	
+    letter-spacing: -0.26px;	
+    line-height: 24px;
+`;
+
+const Img = styled.div`
+    background: url('/images/faq-footer.png') center no-repeat;
+    background-size: cover;
+`;
 
 export default class Footer extends PureComponent {
 
     render() {
-
         return (
-            <div>footer</div>
+            <Flex justify="space-between"
+                  align="center"
+                  wrap="wrap">
+                <Text>
+                    Мы НИКОГДА не попросим ваши приватные ключи или деньги. Если кто-то представляется членами команды
+                    Golos.io и просит такую информацию, напишите нам на почту pr@golos.io.
+                </Text>
+            </Flex>
         );
     }
 }
