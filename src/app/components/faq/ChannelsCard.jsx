bf719599cdfe0486ee78e423ffcac2bb7fe2ff93
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-08-15 15:04:51 +0300

    refactoring footer and channels components in faq

---
+++ src/app/components/faq/ChannelsCard.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router';
+import Icon from '../golos-ui/Icon/Icon';
+
+const LinkTo = ({ children, className, link }) => {
+    return (
+        <Link to={link} target="_blank" className={className}>
+            {children}
+        </Link>
+    );
+};
+
+const Card = styled(LinkTo)`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 214px;
+    height: 80px;
+    padding: 0 20px;
+    border-radius: 8.53px;
+    background-color: #ffffff;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.06);
+    margin: 10px;
+    cursor: pointer;
+`;
+
+const Text = styled.p`
+    text-transform: uppercase;
+    color: #393636;
+    margin: 0 0 0 22px;
+    font-size: 12px;
+    font-family: 'Open Sans', sans-serif;
+    letter-spacing: 0.37px;
+    line-height: 18px;
+    font-weight: bold;
+`;
+
+const CustomIcon = Icon.extend.attrs({
+    width: props => props.width,
+    height: props => props.height,
+    name: props => props.name,
+})`
+    min-width: ${props => props.width}px;
+`;
+
+export default class ChannelsCard extends Component {
+    render() {
+        const { inscription, thumbnail, width, height, link } = this.props.card;
+        return (
+            <Card link={link}>
+                <CustomIcon name={thumbnail} width={width} height={height} />
+                <Text>{inscription}</Text>
+            </Card>
+        );
+    }
+}
--- src/app/components/faq/Footer.jsx
+++ src/app/components/faq/Footer.jsx
@@ -1,33 +1,64 @@
-import React, {PureComponent} from 'react';
-import Flex from '../Flex/Flex';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
+import Flex from '../Flex/Flex';
 
-const Text = styled.p`
-    color: #393636;	
-    font-family: "SF Pro Text", "Roboto", sans-serif;	
-    font-size: 16px;	
-    font-weight: 500;	
-    letter-spacing: -0.26px;	
-    line-height: 24px;
+const Wrapper = Flex.extend`
+    background: #ffffff;
+    padding: 20px 64px;
+
+    @media (max-width: 830px) {
+        padding: 0;
+        flex-direction: column;
+        margin-bottom: 40px;
+    }
 `;
 
-const Img = styled.div`
-    background: url('/images/faq-footer.png') center no-repeat;
-    background-size: cover;
+const Text = styled.p`
+    width: 50%;
+    color: #393636;
+    margin: 0;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: -0.26px;
+
+    @media (max-width: 830px) {
+        width: 100%;
+        max-width: 600px;
+        padding: 40px 24px 20px 24px;
+    }
+`;
+
+const Image = styled.div`
+    flex-grow: 1;
+    height: 177px;
+    background: url('/images/new/faq/footer.svg') 53% 50% no-repeat;
+    background-size: 242px 177px;
+
+    @media (max-width: 830px) {
+        width: 100%; 
+        height: 238px;
+        background-position: center;
+        background-size: auto 238px;
+    }
+
+    @media (max-width: 430px) {
+        height: 73vw;
+        background-size: 80vw 73vw;
+    }
 `;
 
 export default class Footer extends PureComponent {
-
     render() {
         return (
-            <Flex justify="space-between"
-                  align="center"
-                  wrap="wrap">
+            <Wrapper justify="space-between" align="center">
                 <Text>
-                    Мы НИКОГДА не попросим ваши приватные ключи или деньги. Если кто-то представляется членами команды
-                    Golos.io и просит такую информацию, напишите нам на почту pr@golos.io.
+                    Мы НИКОГДА не попросим ваши приватные ключи или деньги. Если
+                    кто-то представляется членами команды Golos.io и просит
+                    такую информацию, напишите нам на почту pr@golos.io.
                 </Text>
-            </Flex>
+                <Image />
+            </Wrapper>
         );
     }
 }
