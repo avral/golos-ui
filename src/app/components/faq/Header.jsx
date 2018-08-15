b0e278859b5e4b345710af370012dfe039b3a368
dmitryorelopt <dmitryorelopt@gmail.com>
2018-08-15 11:35:37 +0300

    added media for header

--- src/app/components/faq/Header.jsx
+++ src/app/components/faq/Header.jsx
@@ -2,10 +2,27 @@ import React, {PureComponent} from 'react';
 import styled from 'styled-components';
 import Button from '../golos-ui/Button/Button';
 import Icon from '../golos-ui/Icon';
+import Flex from '../Flex';
 
-const Wrapper = styled.div`
+const Wrapper = Flex.extend.attrs({
+    auto: true
+})`
+    background-color: #f9f9f9;
+    
+    @media (max-width: 1200px) {
+        flex-direction: column-reverse;
+        align-items: center;
+    }
+`;
+
+const Body = styled.div`
     padding: 60px;
-    background: url('/images/new/faq/header-beck.png') no-repeat right #f9f9f9;
+    
+    @media (max-width: 1200px) {
+        max-width: 100%;
+        padding: 25px 16px 40px;
+        text-align: center;
+    }
 `;
 
 const Title = styled.h1`
@@ -25,12 +42,20 @@ const Description = styled.div`
     font-family: "SF Pro Text";	
     font-size: 16px;	
     letter-spacing: -0.26px;	
-    line-height: 24px;
+    line-height: 24px; 
+    
+    @media (max-width: 1200px) {
+        margin-bottom: 20px;
+    }
 `;
 
 const ButtonsBlock = styled.div`
     display: flex;
     justify-content: flex-start;
+   
+    @media (max-width: 1200px) {
+        justify-content: center;
+    }
 `;
 
 const BlueButton = Button.extend`
@@ -42,6 +67,10 @@ const BlueButton = Button.extend`
     line-height: 18px;	
     text-shadow: 0 2px 4px 0 rgba(0,0,0,0.5), 0 2px 12px 0 rgba(0,0,0,0.15);
     letter-spacing: 1px;
+    
+    @media (max-width: 1200px) {
+        margin-left: 15px;
+    }
 `;
 
 const WhiteButton = Button.extend`
@@ -61,6 +90,26 @@ const WhiteButton = Button.extend`
     &:hover {
         background-color: #fff;
     }
+    
+    @media (max-width: 1200px) {
+        margin-left: 0px;
+    }
+`;
+
+const Image = styled.div`
+    flex-grow: 1;
+    background: url('/images/new/faq/header-back.svg') no-repeat center;
+    background-size: 451px 280px;
+    
+    @media (max-width: 1200px) {
+        width: 100%;
+        height: 280px;
+    }
+    
+    @media (max-width: 450px) {
+        height: 58vw;
+        background-size: 100vw 58vw;
+    }
 `;
 
 export default class Header extends PureComponent {
@@ -69,6 +118,7 @@ export default class Header extends PureComponent {
 
         return (
             <Wrapper>
+                <Body>
                 <Title>Часто задаваемые вопросы</Title>
                 <Description>На этой странице собраны самые популярные вопросы пользователей, обращавшихся в
                     техподдержку Golos.io
@@ -81,6 +131,8 @@ export default class Header extends PureComponent {
                         <Icon name="envelope" size="16px"/>Почта
                     </WhiteButton>
                 </ButtonsBlock>
+                </Body>
+                <Image/>
             </Wrapper>
         );
     }
