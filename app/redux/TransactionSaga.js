22e91f734d19493d13d0ea2a15d8f0fac7aa9c5a
avral <qweasd_96@mail.ru>
2019-04-01 09:44:25 +0800

    disable beneficiaries

--- app/redux/TransactionSaga.js
+++ app/redux/TransactionSaga.js
@@ -368,7 +368,8 @@ function* preBroadcast_comment({operation, username}) {
                 percent_steem_dollars,
                 allow_votes,
                 allow_curation_rewards,
-                extensions: /* $STM_Config.isTestnet ? [] : */ [ [ 0, { beneficiaries: [{ account: 'golosio', weight: 1000 }] } ] ]
+                // extensions: /* $STM_Config.isTestnet ? [] : */ [ [ 0, { beneficiaries: [{ account: '', weight: 1000 }] } ] ]
+                extensions: /* $STM_Config.isTestnet ? [] : */ []
             }]
         )
     }
